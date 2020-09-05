14aa507bb0c1371b32391b77d8c8fb62bebdb49a
emmanuel2612 <64177390+emmanuel2612@users.noreply.github.com>
2020-09-05 19:28:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+window.onload = () =>{
+    
+const close = document.getElementById("close");
+const notice = document.querySelector(".notice");
+
+close.onclick = () =>{
+    notice.style.display = "none";
+}
+
+
+
+}
